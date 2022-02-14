6a1eaa45d93455723e831b2ead75b1201c7f0fb9
Derek Spencer <derek.spencer67@gmail.com>
2022-02-13 20:42:24 -0400

    Chapter 4 Complete

--- src/components/ThreadList.vue
+++ src/components/ThreadList.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="col-full">
-
     <div class="thread-list">
 
       <h2 class="list-title">Threads</h2>
@@ -11,7 +10,7 @@
             <router-link :to="{name: 'ThreadShow', params: {id: thread.id}}">{{ thread.title }}</router-link>
           </p>
           <p class="text-faded text-xsmall">
-            By <a href="#">{{ userById(thread.userId).name }}</a>, {{ thread.publishedAt }}.
+            By <a href="#">{{ userById(thread.userId).name }}</a>, <AppDate :timestamp="thread.publishedAt" />.
           </p>
         </div>
 
@@ -26,7 +25,7 @@
             <p class="text-xsmall">
               <a href="#">{{ userById(thread.userId).name }}</a>
             </p>
-            <p class="text-xsmall text-faded">{{ thread.publishedAt }}</p>
+            <p class="text-xsmall text-faded"><AppDate :timestamp="thread.publishedAt" /></p>
           </div>
         </div>
       </div>
@@ -62,4 +61,5 @@ export default {
 </script>
 
 <style scoped>
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
-import PageHome from '@/components/PageHome'
-import PageThreadShow from '@/components/PageThreadShow'
-import PageNotFound from '@/components/PageNotFound'
+import Home from '@/pages/Home'
+import ThreadShow from '@/pages/ThreadShow'
+import NotFound from '@/pages/NotFound'
+import Forum from '@/pages/Forum'
 import { createRouter, createWebHistory } from 'vue-router'
 import sourceData from '@/data.json'
 
@@ -8,41 +9,45 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: PageHome
-},
-{
-  path: '/thread/:id',
-  name: 'ThreadShow',
-  component: PageThreadShow,
-  props: true,
-  beforeEnter (to, from, next) {
-    // check if thread exists
-    const threadExists = sourceData.threads.find(thread => thread.id === to.params.id)
-    // if exists continue
-    if (threadExists) {
-      return next()
-    } else {
-      next({
-        name: 'NotFound',
-        params: { pathMatch: to.path.substring(1).split('/') },
-        // preserve existing query and hash
-        query: to.query,
-        hash: to.hash
-      })
+    component: Home
+  },
+  {
+    path: '/forum/:id',
+    name: 'Forum',
+    component: Forum,
+    props: true
+  },
+  {
+    path: '/thread/:id',
+    name: 'ThreadShow',
+    component: ThreadShow,
+    props: true,
+    beforeEnter (to, from, next) {
+      // check if thread exists
+      const threadExists = sourceData.threads.find(thread => thread.id === to.params.id)
+      // if exists continue
+      if (threadExists) {
+        return next()
+      } else {
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          // preserve existing query and hash
+          query: to.query,
+          hash: to.hash
+        })
+      }
+      // if doesnt exist redirect to not found
     }
-    // if doesnt exist redirect to not found
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFound
   }
-},
-{
-  path: '/:pathMatch(.*)*',
-  name: 'NotFound',
-  component: PageNotFound
-}
 ]
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-    })
-    
-    export default router;
+export default createRouter({
+  history: createWebHistory(),
+  routes
+})
