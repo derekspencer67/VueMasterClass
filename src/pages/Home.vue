4e4b74a831e59275459bf8cc35fc9b599077d2a5
Derek Spencer <derek.spencer67@gmail.com>
2022-02-23 19:59:16 -0400

    Chapter 5 Complete

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <h1>Welcome to the Forum</h1>
-  <ForumList :forums="forums"/>
+  <h1 class="push-top">Welcome to the Forum</h1>
+  <CategoryList :categories="categories"/>
 </template>
 
 <script>
-import ForumList from '@/components/ForumList'
-import sourceData from '@/data.json'
+import CategoryList from '@/components/CategoryList'
 export default {
   components: {
-    ForumList
+    CategoryList
   },
-  data () {
-    return {
-      forums: sourceData.forums
+  computed: {
+    categories () {
+      return this.$store.state.categories
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,34 @@ import Home from '@/pages/Home'
 import ThreadShow from '@/pages/ThreadShow'
 import NotFound from '@/pages/NotFound'
 import Forum from '@/pages/Forum'
+import Category from '@/pages/Category'
 import { createRouter, createWebHistory } from 'vue-router'
 import sourceData from '@/data.json'
+import Profile from '@/pages/Profile'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/me',
+    name: 'Profile',
+    component: Profile,
+    meta: { toTop: true, smoothScroll: true }
+  },
+  {
+    path: '/me/edit',
+    name: 'ProfileEdit',
+    component: Profile,
+    props: { edit: true }
+  },
+  {
+    path: '/category/:id',
+    name: 'Category',
+    component: Category,
+    props: true
   },
   {
     path: '/forum/:id',
@@ -49,5 +69,11 @@ const routes = [
 
 export default createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+  scrollBehavior (to) {
+    const scroll = {}
+    if (to.meta.toTop) scroll.top = 0
+    if (to.meta.smoothScroll) scroll.behavior = 'smooth'
+    return scroll
+  }
 })
